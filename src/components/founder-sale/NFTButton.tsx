260cb75e94a21f790d951b82d2f01de4605ac89d
michal.c <michal.ciezki@binarapps.com>
2024-10-09 16:10:06 +0200

    feat(FE): init founders collection layout

---
+++ src/components/founder-sale/NFTButton.tsx
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+
+import { colors } from 'theme';
+
+interface Props {
+  value: number;
+}
+
+export const NFTButton = ({ value }: Props) => (
+  <GradientButton onClick={() => openPaymentWindow(value)}>
+    {`Buy ${value} Fida Founder’s NFT (${countAda(value)} ADA)`}
+  </GradientButton>
+);
+
+const GradientButton = styled.button`
+  margin-bottom: 40px;
+  border: 1px solid ${colors.blue};
+  background: ${colors.backgroundGradient};
+  color: white;
+  font-size: 18px;
+  font-weight: 700;
+  padding: 10px 20px;
+  cursor: pointer;
+  border-radius: 24px;
+  outline: none;
+  height: 60px;
+  width: 365px;
+  transition: 0.5s;
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:active {
+    opacity: 0.8;
+  }
+
+   @media (max-width: 840px) {
+    width: 60%;
+    font-size: 16px;
+  }
+`;
+
+const countAda = (count: number) => (count * 500) - ((count - 1) * 50);
+
+const openPaymentWindow = (value: number) => {
+  const paymentUrl = `https://pay.nmkr.io/?p=1fe458e78a46451fb812b36ab3fa6f82&c=${value}`;
+
+  const popupWidth = 500;
+  const popupHeight = 700;
+
+  const wtop = window.top ?? { outerWidth: 400, outerHeight: 400, screenX: 800, screenY: 800 };
+  const left = wtop.outerWidth / 2 + wtop.screenX - (popupWidth / 2);
+  const top = wtop.outerHeight / 2 + wtop.screenY - (popupHeight / 2);
+
+  const popup = window.open(paymentUrl, 'NFT-MAKER PRO Payment Gateway', `popup=1, location=1, width=${popupWidth}, height=${popupHeight}, left=${left}, top=${top}`);
+
+  document.body.style.cssText = 'background: rgba(0, 0, 0, 0.5)';
+
+  const backgroundCheck = setInterval(() => {
+    if (popup?.closed) {
+      clearInterval(backgroundCheck);
+      // eslint-disable-next-line no-console
+      console.log('Popup closed');
+
+      document.body.style.cssText = '';
+    }
+  }, 1000);
+};
---
+++ src/components/founder-sale/NFTSlider.tsx
@@ -0,0 +1,70 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { useMediaQuery } from 'react-responsive';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/navigation';
+
+import './styles.css';
+
+import { EffectCoverflow, Navigation } from 'swiper/modules';
+import styled from 'styled-components';
+
+import nft1 from 'assets/images/nft-cards/nft-1.svg';
+import nft2 from 'assets/images/nft-cards/nft-2.svg';
+import nft3 from 'assets/images/nft-cards/nft-3.svg';
+import nft4 from 'assets/images/nft-cards/nft-4.svg';
+import nft5 from 'assets/images/nft-cards/nft-5.svg';
+import nft6 from 'assets/images/nft-cards/nft-6.svg';
+
+import { maxWidth640 } from 'components/rwd/detectMobile';
+
+export const NFTSlider = () => {
+  const isMobile = useMediaQuery({
+    query: maxWidth640,
+  });
+
+  return (
+    <NFTSliderContainer>
+      <Swiper
+        effect="coverflow"
+        grabCursor
+        initialSlide={1}
+        centeredSlides
+        slidesPerView={isMobile ? 2 : 5}
+        coverflowEffect={{
+          rotate: 20,
+          stretch: 20,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        loop
+        pagination={false}
+        navigation
+        modules={[Navigation, EffectCoverflow]}
+        className="mySwiper"
+      >
+        {nfts.map((nft, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+          <SwiperSlide key={`nft-${index}`}>
+            <img src={nft} alt="nft" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </NFTSliderContainer>
+  );
+};
+
+const NFTSliderContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 0 40px;
+
+    @media (max-width: 840px) {
+    padding: 0 12px;
+  }
+`;
+
+const nfts = [nft1, nft2, nft3, nft4, nft5, nft6, nft1, nft2, nft3, nft4, nft5, nft6, nft1, nft2, nft3, nft4, nft5, nft6, nft1, nft2, nft3, nft4, nft5, nft6, nft1, nft2, nft3, nft4, nft5, nft6, nft1, nft2, nft3, nft4, nft5, nft6];
