9ee57d7645ee482bfb68a48b5d00b5725535e4d3
michal.c <michal.ciezki@binarapps.com>
2024-12-05 09:54:16 +0100

    remove unnecessary if for preloading

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,9 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-// Lazy loading with preloading
 const Index = lazy(() => import('../pages/Index'));
 const FounderSale = lazy(() => import('../pages/FounderSale'));
 const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
-
-// Preloading critical routes
-if ('requestIdleCallback' in window) {
-  requestIdleCallback(() => {
-    import('../pages/Index');
-  });
-} else {
-  setTimeout(() => {
-    import('../pages/Index');
-  }, 2000);
-}
 
 const App = () => (
   <Router>
